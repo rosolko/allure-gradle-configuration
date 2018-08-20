bdff739a2ef9721e993d8254eb9ce86f9f001d27
Aliaksandr Rasolka <rosolko@gmail.com>
2018-08-20 10:02:13 +0300

    Update SelenideTests.java

--- src/test/java/SelenideTests.java
+++ src/test/java/SelenideTests.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
